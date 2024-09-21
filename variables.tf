09a079b8503d860fe863f2dbee7f85868d5bc060
shon <yashoncarmack@gmail.com>
2024-09-21 09:48:57 -0400

    added the provider and variables tf files

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# variables.tf
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "terraform_ansible_project" {
+  description = "Project name prefix"
+  default     = "tf-ansible-demo"
+}
+
+variable "public_key_path" {
+  description = "Path to your public SSH key"
+  default     = "~/.ssh/id_rsa.pub"
+}
