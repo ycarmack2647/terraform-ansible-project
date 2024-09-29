21f65a5d83f8cf1b472d4bc38c2d818556fa22a0
Yashon <114156150+ycarmack2647@users.noreply.github.com>
2024-09-29 10:29:56 -0400

    Update key_pair.tf

--- key_pair.tf
+++ key_pair.tf
@@ -1,5 +1,18 @@
 # key_pair.tf
+
+variable "public_key_path" {
+  description = "Path to your Ansible public SSH key"
+  default     = "~/.ssh/id_rsa.pub" # Update this to the path of your Ansible public key
+}
+
+variable "key_pair_name" {
+  description = "The name of the existing key pair for SSH access"
+  type        = string
+  default     = "mykey" # Your key pair name for SSH access ansible server
+}
+
+
 resource "aws_key_pair" "admin_key" {
-  key_name   = "${var.project_name}-key"
-  public_key = file(var.public_key_path)
+  key_name   = "admin_key1"
+  public_key = file("~/.ssh/id_rsa.pub") # Update the path to your public key web servers
 }
