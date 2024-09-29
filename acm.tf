94e1d4aa07c9c8b4c310a94e7d9268bc9358c798
Yashon <114156150+ycarmack2647@users.noreply.github.com>
2024-09-29 10:39:10 -0400

    Create acm.tf

---
+++ acm.tf
@@ -0,0 +1,19 @@
+# Replace with your domain name
+variable "domain_name" {
+  type    = string
+  default = "shonproject101.com"
+}
+
+# Data source to find the existing ACM certificate by domain name
+data "aws_acm_certificate" "existing_cert" {
+  provider    = aws.us_east_1
+  domain      = var.domain_name
+  statuses    = ["ISSUED"]
+  most_recent = true  # Gets the most recently issued certificate
+}
+
+# Alternatively, if you know the certificate ARN, you can use:
+# data "aws_acm_certificate" "existing_cert" {
+#   provider = aws.us_east_1
+#   arn      = "aws:acm:us-east-1:accountnumber:certificate" # Put your arn for the acm
+# }
