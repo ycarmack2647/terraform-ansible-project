81e9fd11939ad61b14115d4647826c60ba53f8db
Yashon <114156150+ycarmack2647@users.noreply.github.com>
2024-09-29 11:30:10 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,10 @@
-# provider.tf
+# Default AWS provider configuration (replace with your default region)
 provider "aws" {
+  region = "us-east-1"  # e.g., "us-west-2"
+}
+
+# Provider configuration for the us-east-1 region
+provider "aws" {
+  alias  = "us_east_1"
   region = "us-east-1"
 }
